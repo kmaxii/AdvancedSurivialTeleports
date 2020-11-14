be2bedc1b2cc38cdf4207499adb64ef5ef24f960
kmaxiyt <esst936@gmail.com>
2020-11-14 16:03:55 +0100

    Started development

---
+++ src/main/java/me/kmaxi/advancedSurvivalTeleports/TeleportsMain.java
@@ -0,0 +1,15 @@
+package me.kmaxi.advancedSurvivalTeleports;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TeleportsMain extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+}
---
+++ src/main/java/me/kmaxi/advancedSurvivalTeleports/commands/CommanadsManager.java
@@ -0,0 +1,15 @@
+package me.kmaxi.advancedSurvivalTeleports.commands;
+
+import me.kmaxi.advancedSurvivalTeleports.TeleportsMain;
+
+public class CommanadsManager {
+
+    private final TeleportsMain plugin;
+
+    public CommanadsManager(TeleportsMain plugin){
+        this.plugin = plugin;
+    }
+
+
+
+}
